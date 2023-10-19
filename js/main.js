5b8e6af57825284ab7fee43422eb407861267f0a
mcgmark <mcgmark14@icloud.com>
2023-10-18 21:01:50 -0400

    Initial Commit - Repost

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$( document ).ready(function() {
+$("#menuButton").on("click", function(){
+    $("#mobile-menu").toggleClass("show");
+    $("#menuButton i").toggleClass("rotate");
+});
+// let current = $("#counter section:nth-of-type(2) h2").text();
+// current = parseInt(current);
+});
+
+
